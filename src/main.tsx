3c43638f36bd5e3187d8b550385d66b9e677b1e5
Ely Hawkins <44911472+Wheelsy@users.noreply.github.com>
2025-08-26 13:20:24 +1000

    analytics
    added anaytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App";
 import theme from "./theme";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <Analytics />
       <CssBaseline />
       <BrowserRouter>
         <App />
