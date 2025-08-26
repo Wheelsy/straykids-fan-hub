58f69117a6bac49b34446805da6bcaa54dd6275a
Ely Hawkins <44911472+Wheelsy@users.noreply.github.com>
2025-08-26 13:27:01 +1000

    undo analytics change

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App";
 import theme from "./theme";
-import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Analytics />
       <CssBaseline />
       <BrowserRouter>
         <App />
